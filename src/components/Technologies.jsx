7fa3d8d4c86173f929ebac35f5da13b309602d66
SebaLaguna <133886721+SebaLaguna@users.noreply.github.com>
2024-10-14 19:00:36 -0300

    ..

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 const Technologies = () => {
   const technologies = [
-    { name: "React JS", imgSrc: "./public/assets/react.png" },
-    { name: "Java", imgSrc: "./public/assets/java.png" },
-    { name: "C#", imgSrc: "/public/assets/csharp.png" },
-    { name: ".NET", imgSrc: "/public/assets/dotnet.png" },
-    { name: "Python", imgSrc: "/public/assets/python.png" },
-    { name: "React Vite", imgSrc: "/public/assets/vite.png" },
-    { name: "Spring Boot", imgSrc: "/public/assets/spring.png" },
-    { name: "MongoDB", imgSrc: "/public/assets/mongodb.png" },
-    { name: "SQL", imgSrc: "/public/assets/sql.png" },
-    { name: "MySQL", imgSrc: "/public/assets/mysql.png" },
-    { name: "Visual Studio", imgSrc: "/public/assets/visualstudio.png" }
+    { name: "React JS", imgSrc: "./images/react.png" },
+    { name: "Java", imgSrc: "./images/java.png" },
+    { name: "C#", imgSrc: "./images/csharp.png" },
+    { name: ".NET", imgSrc: "./images/dotnet.png" },
+    { name: "Python", imgSrc: "./images/python.png" },
+    { name: "React Vite", imgSrc: "./images/vite.png" },
+    { name: "Spring Boot", imgSrc: "./images/spring.png" },
+    { name: "MongoDB", imgSrc: "./images/mongodb.png" },
+    { name: "SQL", imgSrc: "./images/sql.png" },
+    { name: "MySQL", imgSrc: "./images/mysql.png" },
+    { name: "Visual Studio", imgSrc: "./images/visualstudio.png" }
   ];
 
+  console.log("cargando foto ",technologies[0].imgSrc );
   return (
     <div className="technologies-container text-center" id='technologies'>
       <h2 className="text-orange">Technologies</h2>
       <div className="icon-container">
         {technologies.map((tech, index) => (
+            
           <div className="tech-icon" key={index}>
             <img src={tech.imgSrc} alt={tech.name} className="icon" />
             <span className="icon-label">{tech.name}</span>
