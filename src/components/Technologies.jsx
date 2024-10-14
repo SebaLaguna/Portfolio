e2c586ec1c5f6a1de09bead607ea953f6ef03106
SebaLaguna <133886721+SebaLaguna@users.noreply.github.com>
2024-10-14 17:05:11 -0300

    cambio fotos de ruta

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Technologies = () => {
   const technologies = [
-    { name: "React JS", imgSrc: "/src/components/images/react.png" },
-    { name: "Java", imgSrc: "/src/components/images/java.png" },
-    { name: "C#", imgSrc: "/src/components/images/csharp.png" },
-    { name: ".NET", imgSrc: "/src/components/images/dotnet.png" },
-    { name: "Python", imgSrc: "/src/components/images/python.png" },
-    { name: "React Vite", imgSrc: "/src/components/images/vite.png" },
-    { name: "Spring Boot", imgSrc: "/src/components/images/spring.png" },
-    { name: "MongoDB", imgSrc: "/src/components/images/mongodb.png" },
-    { name: "SQL", imgSrc: "/src/components/images/sql.png" },
-    { name: "MySQL", imgSrc: "/src/components/images/mysql.png" },
-    { name: "Visual Studio", imgSrc: "/src/components/images/visualstudio.png" }
+    { name: "React JS", imgSrc: "/public/react.png" },
+    { name: "Java", imgSrc: "/public/java.png" },
+    { name: "C#", imgSrc: "/public/csharp.png" },
+    { name: ".NET", imgSrc: "/public/dotnet.png" },
+    { name: "Python", imgSrc: "/public/python.png" },
+    { name: "React Vite", imgSrc: "/public/vite.png" },
+    { name: "Spring Boot", imgSrc: "/public/spring.png" },
+    { name: "MongoDB", imgSrc: "/public/mongodb.png" },
+    { name: "SQL", imgSrc: "/public/sql.png" },
+    { name: "MySQL", imgSrc: "/public/mysql.png" },
+    { name: "Visual Studio", imgSrc: "/public/visualstudio.png" }
   ];
 
   return (
